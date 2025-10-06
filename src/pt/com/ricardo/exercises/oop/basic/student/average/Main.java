8fb05f014ef0437897af5d0a6bec44b05921894f
rcd_olsen <rcd_olsen@yahoo.com.br>
2025-10-06 12:20:19 +0100

    included getters and setters and removed the the unnecessary i in the for loop in main

--- src/pt/com/ricardo/exercises/oop/basic/student/average/Main.java
+++ src/pt/com/ricardo/exercises/oop/basic/student/average/Main.java
@@ -1,11 +1,12 @@
 package pt.com.ricardo.exercises.oop.basic.student.average;
 
 
+import java.util.Arrays;
+
 public class Main {
-    public static <students> void main(String[] args) {
+    public static void main(String[] args) {
 
         MakeStudentAttributes[] students = new MakeStudentAttributes[4];
-        int i = 0;
 
 //        Parse the students data to the MakeStudentsAttributes class to stores it in objects in an array
         students[0] = new MakeStudentAttributes("Ricardo", 37, 8, 10, 15);
@@ -13,11 +14,10 @@ public class Main {
         students[2] = new MakeStudentAttributes("Mozuda", 38, 8, 0, 2.5);
         students[3] = new MakeStudentAttributes("Test", 0, 9.999999, 9.99, 9.99);
 
-//        iterates over the array with the students data created to show all of their info
+//        iterates over the array with the students data to show all of their info
         for (MakeStudentAttributes student : students) {
-
-            students[i].showInfo();
-            i += 1;
+            student.showInfo();
         }
+
     }
 }
--- src/pt/com/ricardo/exercises/oop/basic/student/average/MakeStudentAttributes.java
+++ src/pt/com/ricardo/exercises/oop/basic/student/average/MakeStudentAttributes.java
@@ -6,22 +6,64 @@ public class MakeStudentAttributes {
 
     private String name;
     private int age;
-    private double[] grades = new double[] {0, 0, 0};
+    private double[] grades = new double[]{0, 0, 0};
     private double average;
     private String situation;
+
+//    getters and setters start
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setGrades(double[] grades) {
+        this.grades = grades;
+    }
+
+    public double[] getGrades() {
+        return grades;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+//    getters and setters end
 
     public MakeStudentAttributes(String name, int age, double grade1, double grade2, double grade3) {
 
 //        stores the data parsed in the object attributes
-        this.name = name;
-        this.age = age;
-        this.grades = new double[]{grade1, grade2, grade3};
-        this.average = Math.round(this.average() * 10.0) / 10.0;
-        this.situation = this.situation(this.average);
+        this.setName(name);
+        this.setAge(age);
+        this.setGrades(new double[]{grade1, grade2, grade3});
+        this.setAverage(Math.round(this.calculateAverage() * 10.0) / 10.0);
+        this.setSituation(this.determineSituation(this.average));
     }
 
     //    iterates over the grades in the list and returns the average grade to the object`s average attribute
-    protected double average() {
+    protected double calculateAverage() {
         double average = 0;
         int i = 0;
 
@@ -29,26 +71,26 @@ public class MakeStudentAttributes {
             average += grade;
             i++;
         }
+
         if (i > 0) return average / i;
         else return 0;
     }
 
     //    Uses the average grade to show if the student has passed or failed
-    protected String situation(double grade) {
+    protected String determineSituation(double grade) {
         if (grade < 10) {
             return "failed";
         }
         return "passed";
     }
 
-    //    Method to show the students stored info
-    protected void showInfo() {
-        System.out.println("name: " + name);
-        System.out.println("age: " + age);
-        System.out.println(Arrays.toString(this.grades));
-        System.out.println("Average rounded grade: " + this.average);
-        System.out.println("Situation: " + this.situation);
-
+    // Show all student info
+    public void showInfo() {
+        System.out.println("name: " + this.getName());
+        System.out.println("age: " + this.getAge());
+        System.out.println(Arrays.toString(this.getGrades()));
+        System.out.println("Average rounded grade: " + this.getAverage());
+        System.out.println("Situation: " + this.getSituation());
         System.out.println();
     }
 }
